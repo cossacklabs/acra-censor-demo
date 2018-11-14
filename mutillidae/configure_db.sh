eaeea96b5acaa9978d0fb4ee2299ab7bf25e3c14
Dmytro Shapovalov <shad@shad.in.ua>
2018-11-14 12:59:57 +0200

    Improved infrastructure of the demo project (#1)
    * Improved infrastructure of the demo project
    
    * removed `edoz90/mutillidae` repository clone
    * pre-build base image `edoz90_mutillidae` using github as source
    * build `mutillidae` image based on `edoz90_mutillidae`
    * use the one docker-compose file instead of multi-step launch procedure
    * use private networks to show links between components
    * optimized MySQL bootstraping
    * optimized MySQL configuration
    * created pre-defined configs for AcraCensor
    * simplified demo process
    * updated README

---
+++ mutillidae/configure_db.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+MYSQL_ROOT_PASSWORD='test'
+
+MYSQL_DB='mutillidae'
+MYSQL_USER='mutillidae'
+MYSQL_PASSWORD='mutillidae'
+
+cat <<EOF | /usr/bin/mysqld --user=mysql --bootstrap --verbose=0
+USE mysql;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' identified by '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;
+UPDATE user SET password=PASSWORD("${MYSQL_ROOT_PASSWORD}") WHERE user='root' AND host='localhost';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DB} CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL ON ${MYSQL_DB}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+
+cat <<EOF > /usr/share/nginx/html/mutillidae/includes/database-config.php
+<?php
+define('DB_HOST', 'acra-connector');
+define('DB_USERNAME', '${MYSQL_USER}');
+define('DB_PASSWORD', '${MYSQL_PASSWORD}');
+define('DB_NAME', '${MYSQL_DB}');
+?>
+EOF
+
+cat <<EOF > /etc/my.cnf.d/mariadb-server.cnf
+[mysqld]
+bind-address=0.0.0.0
+EOF
